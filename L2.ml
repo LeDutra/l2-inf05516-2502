341ad17d29317aa8c092b3ca00b35ea1347750e4
Leonardo Dutra <leo.togepi@gmail.com>
2025-12-07 15:02:43 -0300

    changes

--- L2.ml
+++ L2.ml
@@ -1,58 +1,333 @@
 
+(* ========================================================================== *)
+(*                        DEFINIÇÕES DA LINGUAGEM L2                          *)
+(* ========================================================================== *)
 
-type bop =  
-  | Sum | Sub | Mul | Div   (* operações aritméticas *)
-  | Eq  | Neq | Lt | Gt   (* operações relacionais  *)
-  | And | Or   (* operações lógicas *) 
+(* Tipos de Operadores Binários *)
+type bop = 
+  | Sum | Sub | Mul | Div               (* Aritméticos: +, -, *, / *)
+  | Eq  | Neq | Lt  | Gt                (* Relacionais: =, <>, <, > *)
+  | And | Or                            (* Lógicos: &&, || *)
 
+(* Tipos da Linguagem *)
 type tipo = 
-  | TyInt
-  | TyBool
-  | TyRef of tipo
-  | TyUnit
+  | TyInt                               (* Inteiro: int *)
+  | TyBool                              (* Booleano: bool *)
+  | TyRef of tipo                       (* Referência (Ponteiro): ref T *)
+  | TyUnit                              (* Unitário (Comando): unit *) 
     
-
+(* Árvore de Sintaxe Abstrata (AST) *)
 type expr = 
-  | Num of int
-  | Bool of bool 
-  | Id of string
-  | If of expr * expr * expr 
-  | Binop of bop * expr * expr
-  | Wh of expr * expr 
-  | Asg of expr * expr 
-  | Let of string * tipo * expr * expr 
-  | New of expr
-  | Deref of expr 
-  | Unit
-  | Seq of expr * expr
-  
-          
-          (*      TESTE - fatorial      
-           
-            let  x: int     =  5 in 
-            let  z: ref int = new x in 
-            let  y: ref int = new 1 in 
-            
-            (while (!z > 0) (
-                   y :=  !y * !z;
-                   z :=  !z - 1);
-            ! y)     
+  | Num of int                          (* Números inteiros: 1, 42 *)
+  | Bool of bool                        (* Booleanos: true, false *)
+  | Id of string                        (* Variáveis (Identifiers): x, y *)
+  | Binop of bop * expr * expr          (* Operação Binária: e1 op e2 *)
+  | If of expr * expr * expr            (* Condicional: if e1 then e2 else e3 *)
+  | Let of string * tipo * expr * expr  (* Declaração: let x:T = e1 in e2 *)
+  | Wh of expr * expr                   (* Loop: while e1 do e2 *)
+  | Asg of expr * expr                  (* Atribuição (Assign): e1 := e2 *)
+  | New of expr                         (* Alocação: new e *)
+  | Deref of expr                       (* Desreferência: !e *)
+  | Seq of expr * expr                  (* Sequência: e1; e2 *)
+  | Unit                                (* Valor Unitário: () *)
+  | Loc of int                          (* Alocações de memória (apenas em tempo de execução) *)
 
+(* ========================================================================== *)
+(*                           INFERÊNCIA DE TIPOS                              *)
+(* ========================================================================== *)
+
+exception TypeError of string (* Exceção para erros de tipo encontrados *)
+type tyenv = (string * tipo) list (* Γ Ambiente de tipos *)
+
+(* Função auxiliar para converter tipos em string (para mensagens de erro) *)
+let rec type_to_string t = 
+  match t with
+  | TyInt -> "int"
+  | TyBool -> "bool"
+  | TyRef t -> "ref " ^ type_to_string t
+  | TyUnit -> "unit"
+  | _ -> raise (TypeError "Tipo desconhecido na conversão para string") (* Essa função já cobre todas as possibilidades, mas por segurança checamos se é válida*)
+
+(* Busca o tipo de uma variável no contexto (Lookup) *)
+let rec lookup (env : tyenv) (x : string) : tipo = 
+  match env with 
+  | [] -> raise (TypeError ("Variável não declarada: " ^ x))
+  | (y, t) :: rest -> if x = y then t else lookup rest x
+
+(* Função Principal de Inferência: verifica as regras Γ ⊢ e : T *)
+let rec typeinfer (env : tyenv) (e : expr) : tipo = 
+  match e with
+
+  (* T-INT *)
+  | Num _ -> TyInt
+
+  (* T-BOOL *)
+  | Bool _ -> TyBool
+
+  (* Operadores Binários *)
+  | Binop (op, e1, e2) ->
+      let t1 = typeinfer env e1 in
+      let t2 = typeinfer env e2 in
+      (match op with 
+
+      (* T-OP+, T-OP-, T-OP*, T-OP/ *)
+      | Sum | Sub | Mul | Div ->
+          if t1 = TyInt && t2 = TyInt then TyInt
+          else raise (TypeError "Operações aritiméticas exigem que e1 e e2 sejam inteiros")
+
+      (* T-OP<, T-OP>, T-OP=, T-OP!= *)
+      | Lt | Gt | Eq | Neq ->
+          if t1 = t2 then TyBool
+          else raise (TypeError "Comparações entre e1 e e2 exigem que ambos sejam do mesmo tipo")
+          
+      (* T-OPAND, T-OPOR *)
+      | And | Or ->
+          if t1 = TyBool && t2 = TyBool then TyBool
+          else raise (TypeError "Operações lógicas esperam booleanos"))
+           
+  (* T-IF *)
+  | If (cond, e_then, e_else) ->
+      if typeinfer env cond = TyBool then
+        let t2 = typeinfer env e_then in
+        let t3 = typeinfer env e_else in
+        if t2 = t3 then t2
+        else raise (TypeError ("Tipos dos ramos 'then' e 'else' não são iguais"))
+      else raise (TypeError "Condição do if deve ser booleana")
+
+  (* T-VAR *)
+  | Id x -> lookup env x (* x : T (se x está no contexto) *)
+
+  (* T-LET *)
+  | Let (x, t_decl, e1, e2) ->
+      let t1 = typeinfer env e1 in
+      if t1 = t_decl then 
+        typeinfer ((x, t_decl) :: env) e2 (* Estende o contexto Γ com x:T e infere e2, x |→ T *)
+      else raise (TypeError ("Tipo de e1 não corresponde ao tipo declarado para " ^ x))
+  
+  (* T-ATR *)
+  | Asg (e1, e2) ->
+      let t1 = typeinfer env e1 in
+      let t2 = typeinfer env e2 in
+      if t1 = TyRef t2 then TyUnit
+      else raise (TypeError ("O tipo ref T de e1 precisa ter o ser do mesmo tipo T de e2"))
+
+  (* T-DEREF *)
+  | Deref e ->
+      (match typeinfer env e with 
+       | TyRef t -> t (* Se for um tipo de referência, retorna o tipo referenciado *)
+       | _ -> raise (TypeError "Operação de desreferência exige um tipo ref T"))
+
+  (* T-NEW *)
+  | New e ->
+      let t = typeinfer env e in
+      TyRef t (* Retorna uma referência para o tipo de e *)
+
+  (* T-UNIT *)
+  | Unit -> TyUnit
+       
+  (* T-WHILE *)
+  | Wh (cond, body) ->
+      if typeinfer env cond = TyBool then
+        if typeinfer env body = TyUnit then TyUnit
+        else raise (TypeError "Corpo do while deve ser do tipo unit")
+      else raise (TypeError "Condição do while deve ser booleana")
+      
+  (* T-SEQ *)
+  | Seq (e1, e2) ->
+      if typeinfer env e1 = TyUnit then typeinfer env e2
+      else raise (TypeError "Primeiro de uma sequência deve ser unit")
+
+  | Loc _ -> raise (TypeError "Alocações de memória não devem aparecer no código fonte")
+
+(* ========================================================================== *)
+(*                        AVALIADOR SMALL-STEP                                *)
+(* ========================================================================== *)
+
+(* Memória *)
+(* Representação da memória como uma lista de pares (local, valor) *)
+type tymem = (int * expr) list
+
+(* Gerenciador de Endereços Únicos *)
+let next_loc = ref 0
+let new_loc () =
+  let l = !next_loc in
+  incr next_loc;
+  l
+
+(* Atualiza o valor em um endereço de memória *)
+let update_tymem (s : tymem) (l : int) (v : expr) : tymem =
+  (l, v) :: List.filter (fun (loc, _) -> loc <> l) s
+
+(* Busca o valor em um endereço de memória *)
+let lookup_tymem (s : tymem) (l : int) : expr =
+  try List.assoc l s
+  with Not_found -> failwith ("Falha de Segmentação: endereço " ^ string_of_int l ^ " não alocado")
+
+(* Verifica se uma expressão já é um valor final (não pode ser mais reduzida) *)
+let is_value e = match e with
+  | Num _ | Bool _ | Unit | Loc _ -> true
+  | _ -> false
+
+(* Substituição: substitui ocorrências de x por v na expressão e ({v/x}e) *)
+let rec subst x v e =
+  match e with
+  | Num _ | Bool _ | Unit | Loc _ -> e
+  | Id y -> if x = y then v else e (* Se achou a variável, troca pelo valor *)
+  | If (e1, e2, e3) -> If (subst x v e1, subst x v e2, subst x v e3)
+  | Binop (op, e1, e2) -> Binop (op, subst x v e1, subst x v e2)
+  | Wh (e1, e2) -> Wh (subst x v e1, subst x v e2)
+  | Asg (e1, e2) -> Asg (subst x v e1, subst x v e2)
+  | Let (y, t, e1, e2) ->
+      let e1' = subst x v e1 in
+      if x = y then Let (y, t, e1', e2) (* Se x foi redeclarado, não substitui no corpo *)
+      else Let (y, t, e1', subst x v e2)
+  | New e1 -> New (subst x v e1)
+  | Deref e1 -> Deref (subst x v e1)
+  | Seq (e1, e2) -> Seq (subst x v e1, subst x v e2)
+
+(* Um Passo da Execução (Small-Step): Transforma (e, s) em (e', s') *)
+let rec step (s : tymem) (e : expr) : (expr * tymem) option =
+  match e with
+  | Num _ | Bool _ | Unit | Loc _ -> None                       (* Valores já estão prontos, não precisa dar passo *)
+  
+  (* Regras OP1 e OP2: Reduz operandos binários *)
+  | Binop (op, e1, e2) when is_value e1 && is_value e2 ->
+      (match op, e1, e2 with
+       | Sum, Num n1, Num n2 -> Some (Num (n1 + n2), s)  
+       | Sub, Num n1, Num n2 -> Some (Num (n1 - n2), s)  
+       | Mul, Num n1, Num n2 -> Some (Num (n1 * n2), s)  
+       | Div, Num n1, Num n2 -> Some (Num (n1 / n2), s)  
+       | Lt,  Num n1, Num n2 -> Some (Bool (n1 < n2), s) 
+       | Gt,  Num n1, Num n2 -> Some (Bool (n1 > n2), s) 
+       | Eq,  Num n1, Num n2 -> Some (Bool (n1 = n2), s) 
+       | Neq, Num n1, Num n2 -> Some (Bool (n1 <> n2), s)
+       | And, Bool b1, Bool b2 -> Some (Bool (b1 && b2), s)
+       | Or,  Bool b1, Bool b2 -> Some (Bool (b1 || b2), s)
+       | _ -> None) (* Tipo errado, por segurança checamos *)
+       
+  | Binop (op, e1, e2) when is_value e1 ->
+      (match step s e2 with
+       | Some (e2', s') -> Some (Binop (op, e1, e2'), s')
+       | None -> None)
+       
+  | Binop (op, e1, e2) ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (Binop (op, e1', e2), s')
+       | None -> None)
+
+  (* Regras IF1, IF2, IF3 *)
+  | If (Bool true, e2, _) -> Some (e2, s)
+  | If (Bool false, _, e3) -> Some (e3, s)
+  | If (e1, e2, e3) ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (If (e1', e2, e3), s')
+       | None -> None)
+
+  (* Regras LET1 e LET2 *)
+  | Let (x, _, v, e2) when is_value v ->
+      Some (subst x v e2, s) (* Substitui x por v em e2 *)
+  | Let (x, t, e1, e2) ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (Let (x, t, e1', e2), s')
+       | None -> None)
+
+  (* Regras NEW e NEW1 *)
+  | New v when is_value v ->
+      let l = new_loc () in
+      Some (Loc l, update_tymem s l v) (* Cria Loc l e salva v na memória *)
+  | New e1 ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (New e1', s')
+       | None -> None)
+
+  (* Regras DEREF e DEREF1 *)
+  | Deref (Loc l) ->
+      Some (lookup_tymem s l, s) (* Lê valor da memória *)
+  | Deref e1 ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (Deref e1', s')
+       | None -> None)
+
+  (* Regras ATR, ATR1, ATR2 *)
+  | Asg (Loc l, v) when is_value v ->
+      Some (Unit, update_tymem s l v) (* Atualiza memória e retorna Unit *)
+  | Asg (Loc l, e2) ->
+      (match step s e2 with
+       | Some (e2', s') -> Some (Asg (Loc l, e2'), s')
+       | None -> None)
+  | Asg (e1, e2) ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (Asg (e1', e2), s')
+       | None -> None)
+
+  (* Regras SEQ e SEQ1 *)
+  | Seq (Unit, e2) -> Some (e2, s) (* Descarta o primeiro comando se já acabou *)
+  | Seq (e1, e2) ->
+      (match step s e1 with
+       | Some (e1', s') -> Some (Seq (e1', e2), s')
+       | None -> None)
+
+  (* Regra WHILE: desenrola o loop uma vez *)
+  | Wh (cond, body) ->
+      Some (If (cond, Seq (body, Wh (cond, body)), Unit), s)
+
+  | Id _ -> None (* Variável livre? Erro de execução ou variável não substituída *)
+
+
+(* Loop de Avaliação: roda 'step' até terminar *)
+let rec eval (s : tymem) (e : expr) : expr * tymem =
+  match step s e with
+  | Some (e', s') -> eval s' e'
+  | None -> (e, s)
+
+(* ========================================================================== *)
+(*                           TESTES E UTILITÁRIOS                             *)
+(* ========================================================================== *)
+
+(* Converte expressão para string para impressão *)
+let rec expr_to_string e = 
+  match e with
+  | Num n -> string_of_int n
+  | Bool b -> string_of_bool b
+  | Unit -> "()"
+  | Loc l -> "loc(" ^ string_of_int l ^ ")"
+  | Id x -> x
+  | Binop _ -> "operacao"
+  | _ -> "expr_complexa"
+
+(* Teste: Fatorial de 5 (Imperativo) *)
+(* 
+   let x : int = 5 in
+   let z : ref int = new x in  (z é um ponteiro para o contador)
+   let y : ref int = new 1 in  (y é um ponteiro para o acumulador)
+   while (!z > 0) do (
+       y := !y * !z;
+       z := !z - 1
+   );
+   !y
 *)
 
+let test_factorial = 
+  let body_loop = Seq(
+      Asg(Id "y", Binop(Mul, Deref(Id "y"), Deref(Id "z"))),
+      Asg(Id "z", Binop(Sub, Deref(Id "z"), Num 1))
+  ) in
+  Let("x", TyInt, Num 5, 
+      Let("z", TyRef TyInt, New (Id "x"), 
+          Let("y", TyRef TyInt, New (Num 1),
+              Seq(Wh(Binop(Gt, Deref(Id "z"), Num 0), body_loop),
+                  Deref (Id "y")))))
 
-let cndwhi = Binop(Gt, Deref (Id "z"),Num 0)
-let asgny = Asg(Id "y", Binop(Mul, Deref (Id "y"),Deref(Id "z")))
-let asgnz = Asg(Id "z", Binop(Sub, Deref (Id "z"),Num 1))
-let bdwhi = Seq(asgny, asgnz) 
-let whi = Wh(cndwhi, bdwhi)
-let prt = Deref (Id "y")
-let seq = Seq(whi, prt)
+let () =
+  print_endline "=== Interpretador L2 ===";
+  try
+    print_string "1. Verificando Tipos (Inferência)... ";
+    let t = typeinfer [] test_factorial in
+    print_endline ("Sucesso! Tipo inferido: " ^ type_to_string t);
     
-let fat = Let("x", TyInt, Num 5, 
-              Let("z", TyRef TyInt, New (Id "x"), 
-                  Let("y", TyRef TyInt, New (Num 1),
-                      seq)))
-    
-    
-
+    print_string "2. Executando programa (Avaliação)... ";
+    let (res, _) = eval [] test_factorial in
+    print_endline ("Resultado Final: " ^ expr_to_string res)
+  with 
+  | TypeError msg -> print_endline ("\nERRO DE TIPO: " ^ msg)
+  | Failure msg -> print_endline ("\nERRO DE EXECUÇÃO: " ^ msg)
---
+++ explicacao_projeto.md
@@ -0,0 +1,106 @@
+# Documentação do Projeto L2 (OCaml)
+## Introdução
+Este documento detalha a implementação do interpretador para a linguagem L2, incluindo a Sintaxe Abstrata, a Inferência de Tipos e o Avaliador Small-Step. O objetivo é fornecer uma explicação clara para a apresentação do trabalho.
+
+---
+
+## 1. Sintaxe Abstrata (AST)
+A "Abstract Syntax Tree" define a estrutura da linguagem. É como definimos a gramática que o OCaml entende.
+
+### Tipos (`type tipo`)
+Define os tipos de dados aceitos na linguagem:
+- `TyInt`: Inteiros (ex: `1`, `5`).
+- `TyBool`: Booleanos (`true`, `false`).
+- `TyUnit`: Representa comandos que não retornam valor útil (como atribuições). É similar ao `void` em C/Java.
+- `TyRef of tipo`: Referências (ponteiros) de memória. `TyRef TyInt` é um ponteiro para um inteiro.
+
+### Expressões (`type expr`)
+Representam o código em si.
+- **Valores Básicos:** `Num` (números), `Bool` (booleanos), `Unit`.
+- **Variáveis:** `Id "x"`.
+- **Operações:** `Binop` (soma, subtração, comparações, etc) e `If`.
+- **Imperativo:**
+  - `Let`: Declaração de variáveis.
+  - `New`: Aloca memória (cria um ponteiro).
+  - `Deref (!)`: Lê o valor dentro de um ponteiro.
+  - `Asg (:=)`: Atribui (escreve) um novo valor na memória.
+  - `Seq (;)`: Sequência de comandos. Executa o primeiro, descarta o resultado, e executa o segundo.
+  - `Wh`: Loop `while`.
+- **Interno:**
+  - `Loc of int`: **Importante!** Isso não existe no código fonte escrito pelo usuário. É criado durante a execução (`New`) para representar um endereço de memória real (ex: endereço 0, endereço 1).
+
+---
+
+## 2. Inferência de Tipos (`infer`)
+Esta função verifica se o programa faz sentido *antes* de rodar (tempo de compilação). Ela segue as regras `Γ ⊢ e : T` do PDF.
+
+- **Entrada:** Um contexto `ctx` (lista de variáveis e seus tipos) e uma expressão `e`.
+- **Saída:** O tipo da expressão (`tipo`) ou um erro (`TypeError`).
+
+### Lógica Chave:
+- **Pattern Matching (`match e with`):** O OCaml verifica qual é o tipo da expressão atual.
+- **Binop:** Verifica se os operandos batem. Soma (`Sum`) exige dois `TyInt`. Comparação (`Lt`) exige dois `TyInt` mas retorna `TyBool`.
+- **Let:** Calcula o tipo do valor (`t1`). Verifica se ele bate com o tipo declarado (`t`). Se sim, adiciona a variável no contexto e continua analisando o corpo.
+- **Ref/Deref/Assign:**
+  - `New e`: Se `e` é `int`, `New e` é `ref int`.
+  - `Deref e (!)`: Se `e` é `ref int`, `!e` retorna `int`.
+  - `Asg (:=)`: Verifica se estamos atribuindo o tipo certo dentro da referência certa.
+
+---
+
+## 3. Avaliador Small-Step (`step` e `eval`)
+Esta parte roda o código. O modelo "Small-Step" significa que resolvemos uma pequena parte da conta de cada vez (ex: `1+1+1` vira `2+1`, depois vira `3`).
+
+### Memória (`store`)
+Como a linguagem tem ponteiros (`ref`), precisamos de uma memória.
+- `type store`: É uma lista de pares `(endereço, valor)`.
+- `next_loc`: Um contador global para gerar novos endereços (0, 1, 2...).
+
+### Substituição (`subst`)
+L2 usa substituição para lidar com variáveis.
+- Quando fazemos `let x = 5 in x + 1`, o avaliador substitui todos os `x` por `5` no corpo. Vira `5 + 1`.
+
+### O Passo (`step`)
+Recebe a memória atual (`s`) e a expressão (`e`). Retorna `Some (nova_expressão, nova_memória)` ou `None` (se terminou ou travou).
+
+**Casos Principais:**
+1.  **Binop (Matemática):** Se os dois lados são valores (números prontos), calcula. Se não, tenta resolver o lado esquerdo ou direito.
+2.  **If:** Se a condição é `true`, retorna o bloco `then`. Se `false`, o `else`.
+3.  **New (Alocação):**
+    - Gera um novo endereço (`new_loc`).
+    - Guarda o valor na memória (`update_store`).
+    - Retorna um `Loc l` (ponteiro) substituindo o comando `new`.
+4.  **Deref (!):** Olha o endereço na memória (`lookup_store`) e devolve o valor guardado.
+5.  **Assign (:=):** Atualiza a memória naquele endereço e retorna `Unit`.
+6.  **While:** Truque clássico de compiladores.
+    - `while c do b` vira `if c then (b; while c do b) else ()`.
+    - Ele se desenrola em um `If`.
+
+### Avaliação Completa (`eval`)
+Fica chamando `step` repetidamente (loop recursivo) até que `step` retorne `None` (quando chega num valor final como um número ou um booleano).
+
+---
+
+## 4. O Exemplo (Fatorial)
+O código define manualmente a AST para calcular o fatorial de 5.
+
+```ocaml
+let fat = Let("x", TyInt, Num 5, 
+            Let("z", TyRef TyInt, New (Id "x"),  (* z aponta para 5 *)
+                Let("y", TyRef TyInt, New (Num 1), (* y aponta para 1 *)
+                    seq)))                         (* Loop do fatorial *)
+```
+Isso é equivalente a:
+```c
+int x = 5;
+int *z = new(x);
+int *y = new(1);
+while (*z > 0) {
+    *y = *y * *z;
+    *z = *z - 1;
+}
+return *y;
+```
+
+---
+**Dica para Apresentação:** Mostre que o interpretador primeiro valida os tipos (garantindo segurança) e depois executa passo-a-passo manipulando a lista `store` como se fosse a RAM do computador.
